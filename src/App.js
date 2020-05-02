6eaa87a5d69345352f318503e68cca0b35223c37
Melvynkoh94 <31084923+Melvynkoh94@users.noreply.github.com>
2020-05-02 23:29:26 +0800

    src/Index.js to include components, fetchData, App.modules.css, async componentDidMount()

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from "react";
+// import Cards from "./components/Cards";
+// import Chart from "./components/Chart";
+// import CountryPicker from "./components/CountryPicker";
+
+import { Cards, Chart, CountryPicker } from "./components";
+import styles from "./App.module.css";
+import { fetchData } from "./api"; // usually when you have index files, dont need to specify the filename 'index.js'. Just the file path to index.js will do
 
 class App extends React.Component {
+  //componentDidMount() If you need to load data from a remote endpoint, this is a good place to instantiate the network request.
+  async componentDidMount() {
+    const data = await fetchData();
+    console.log(data);
+  }
+
   render() {
     return (
-      <div>
-        <h1>Covid=19 App</h1>
+      <div className={styles.container}>
+        <h1>Covid-19 App</h1>
+        <Cards />
+        <CountryPicker />
+        <Chart />
       </div>
     );
   }
