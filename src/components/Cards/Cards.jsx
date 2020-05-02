a26aa11cb579a086cdb7b613146b006b6064b47c
Melvynkoh94 <31084923+Melvynkoh94@users.noreply.github.com>
2020-05-03 01:07:23 +0800

    Implemented Cards with proper css styles, including colour at the bottom of each card, gutters between the cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -8,7 +8,7 @@ import {
 } from "@material-ui/core";
 import styles from "./Cards.module.css";
 import Countup from "react-countup";
-import cx from 'classnames';
+import cx from "classnames"; //css classnames
 
 const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
   // console.log(props);
@@ -22,7 +22,13 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
   return (
     <div className={styles.container}>
       <Grid container spacing={3} justify="center">
-        <Grid item component={Card}>
+        <Grid
+          item
+          component={Card}
+          xs={12}
+          md={3}
+          className={cx(styles.card, styles.infected)}
+        >
           <CardContent>
             <Typography color="textSecondary" gutterBottom>
               Infected from Covid-19
@@ -44,7 +50,13 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
           </CardContent>
         </Grid>
 
-        <Grid item component={Card}>
+        <Grid
+          item
+          component={Card}
+          xs={12}
+          md={3}
+          className={cx(styles.card, styles.recovered)}
+        >
           <CardContent>
             <Typography color="textSecondary" gutterBottom>
               Recovered from Covid-19
@@ -66,7 +78,13 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
           </CardContent>
         </Grid>
 
-        <Grid item component={Card}>
+        <Grid
+          item
+          component={Card}
+          xs={12}
+          md={3}
+          className={cx(styles.card, styles.deaths)}
+        >
           <CardContent>
             <Typography color="textSecondary" gutterBottom>
               Deaths from Covid-19
