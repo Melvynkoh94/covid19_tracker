9e1c497e0b5795b12611bfe3522d3fecd4ad0514
Melvynkoh94 <31084923+Melvynkoh94@users.noreply.github.com>
2020-05-04 00:44:06 +0800

    Successful implementation of line chart to be displayed

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { fetchDailyData } from "../../api"; // it's from index.js in src/api
+import { Line, Bar } from "react-chartjs-2";
+import styles from "./Chart.module.css";
 
+/*What is a Hook? 
+A Hook is a special function that lets you “hook into” React features. For example, useState is a Hook that lets you add React state to function components. We’ll learn other Hooks later.
+
+When would I use a Hook? 
+If you write a function component and realize you need to add some state to it, previously you had to convert it to a class. Now you can use a Hook inside the existing function component. We’re going to do that right now!
+*/
 const Chart = () => {
-  return <h1>Chart</h1>;
+  const [dailyData, setDailyData] = useState([]); // using hook. Initializing our dailyData with an empty array '[]'
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setDailyData(await fetchDailyData());
+    };
+
+    fetchAPI();
+  });
+
+  const lineChart = dailyData.length ? (
+    <Line
+      data={{
+        labels: dailyData.map(({ date }) => date),
+        datasets: [
+          {
+            data: dailyData.map(({ confirmed }) => confirmed),
+            label: "Infected",
+            borderColor: "#3333ff",
+            fill: true,
+          },
+          {
+            data: dailyData.map(({ deaths }) => deaths),
+            label: "Deaths",
+            borderColor: "red",
+            backgroundColor: "rgba(255,0,0,0.5)",
+            fill: true,
+          },
+        ],
+      }}
+    />
+  ) : null;
+
+  return <div className={styles.container}>{lineChart}</div>;
 };
 
 export default Chart;
