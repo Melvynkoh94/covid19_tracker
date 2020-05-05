b8534c5792c0b766c67b9cb0030d01ed3887e507
Melvynkoh94 <31084923+Melvynkoh94@users.noreply.github.com>
2020-05-05 17:05:09 +0800

    Updates

---
+++ precache-manifest.05b9bc7d96e3850e2f1587ddbb369215.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82c842c8ecd747a08b64e0219723d0fb",
+    "url": "/covid19_tracker/index.html"
+  },
+  {
+    "revision": "8c92ab5bf1fa906590fd",
+    "url": "/covid19_tracker/static/css/main.6ea5aecb.chunk.css"
+  },
+  {
+    "revision": "a661f7712cf9d21ac000",
+    "url": "/covid19_tracker/static/js/2.628c439e.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19_tracker/static/js/2.628c439e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c92ab5bf1fa906590fd",
+    "url": "/covid19_tracker/static/js/main.fa2fca86.chunk.js"
+  },
+  {
+    "revision": "1d3ae9c6e63fba0c0b7a",
+    "url": "/covid19_tracker/static/js/runtime-main.bcdbfff0.js"
+  }
+]);
